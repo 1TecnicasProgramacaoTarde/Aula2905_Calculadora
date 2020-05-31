004c165e54215afa8e9db17c7c052ed25ac0f0d8
jessica <jessica@endless>
2020-05-30 22:09:32 -0300

    Commit da Jéh-Subtração simples

--- src/pct/Calculadora.java
+++ src/pct/Calculadora.java
@@ -39,6 +39,22 @@ public class Calculadora {
         
         //Jéssica Subtração
         
+        System.out.println("\n\t\t\t -- Calculadora --\n");
+        //Entrada
+        System.out.println("Subtracao Simples");
+        System.out.print("Informe n1: ");
+        n1 = entrada.nextInt();
+        System.out.print("Informe n2: ");
+        n2 = entrada.nextInt();
+        
+        //Processamento
+        total = n1 - n2;
+        
+        
+        //Saída
+        System.out.println("\n\t " + n1 + " - " + n2 + " = " + total);
+        
+        
         
         
         
