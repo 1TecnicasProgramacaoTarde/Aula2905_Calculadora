213abb3e8f51a12938f24e4c2512724fa5cab4e3
B1-08 <B1-08@B1-08-PC>
2020-06-05 16:43:08 -0300

    Commite do tio 2.0! Dia 05 de junho!

--- src/pct/Calculadora.java
+++ src/pct/Calculadora.java
@@ -14,82 +14,91 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         //Declaração de variáveis
-        int n1, n2, total;
+        int n1, n2, total, op;
         
         //Objeto de entrada
         Scanner entrada = new Scanner(System.in);
         
+        //Apresentação
         System.out.println("\n\t\t\t -- Calculadora --\n");
-        //Entrada
-        System.out.println("Soma Simples");
-        System.out.print("Informe n1: ");
-        n1 = entrada.nextInt();
-        System.out.print("Informe n2: ");
-        n2 = entrada.nextInt();
         
-        //Processamento
-        total = n1 + n2;
+        //Menu
+        System.out.println("[1] soma");
+        System.out.println("[2] multiplicação");
+        System.out.println("[3] subtração");
+        System.out.println("[4] resto inteiro da divisao");
+        //System.out.println("[5] divisão"); Daniel
         
+        //Opção do usuário
+        System.out.print("\nOpção: ");
+        op = entrada.nextInt();
         
-        //Saída
-        System.out.println("\n\t " + n1 + " + " + n2 + " = " + total);
+        if(op == 1){
+            //Entrada
+            System.out.println("Soma Simples");
+            System.out.print("Informe n1: ");
+            n1 = entrada.nextInt();
+            System.out.print("Informe n2: ");
+            n2 = entrada.nextInt();
         
-        
-        //Gustavo & Henrique M. Multiplicação
-       
-        //Entrada
-        System.out.println("Multiplicação");
-        System.out.print("Informe n1: ");
-        n1 = entrada.nextInt();
-        System.out.print("Informe n2: ");
-        n2 = entrada.nextInt();
-        
-        //Processamento
-        total = n1 * n2;
-        
-        //Saida
-        System.out.println("\n\t " + n1 + " * " + n2 + " = " + total);
-        
-        
-        //Jéssica Subtração
-        //Acho que repetir o título é desnecessario - D. Ps:D = Douglas :)
-        System.out.println("\n\t\t\t -- Calculadora --\n");
-        //Entrada
-        System.out.println("Subtracao Simples");
-        System.out.print("Informe n1: ");
-        n1 = entrada.nextInt();
-        System.out.print("Informe n2: ");
-        n2 = entrada.nextInt();
-        
-        //Processamento
-        total = n1 - n2;
-        
-        
-        //Saída
-        System.out.println("\n\t " + n1 + " - " + n2 + " = " + total);
-        
-        //Douglas Resto da Divisão
-       
-        //Entrada
-        System.out.println("Resto da divisão");
-        System.out.print("Informe n1: ");
-        n1 = entrada.nextInt();
-        System.out.print("Informe n2: ");
-        n2 = entrada.nextInt();
-        
-        //Processamento
-        total = n1 % n2;
-        
-        //Saida
-        System.out.println("\n\t " + n1 + " % " + n2 + " = " + total);
-        
+            //Processamento
+            total = n1 + n2;
                 
+            //Saída
+            System.out.println("\n\t " + n1 + " + " + n2 + " = " + total);
+        }else if(op == 2){
+                
+            //Gustavo & Henrique M. Multiplicação
+       
+            //Entrada
+            System.out.println("Multiplicação");
+            System.out.print("Informe n1: ");
+            n1 = entrada.nextInt();
+            System.out.print("Informe n2: ");
+            n2 = entrada.nextInt();
+        
+            //Processamento
+            total = n1 * n2;
+        
+            //Saida
+            System.out.println("\n\t " + n1 + " * " + n2 + " = " + total);
+        
+        } else if(op == 3){
+            //Jéssica Subtração
+            //Acho que repetir o título é desnecessario - D. Ps:D = Douglas :)
+            //System.out.println("\n\t\t\t -- Calculadora --\n");
+            //Entrada
+            System.out.println("Subtracao Simples");
+            System.out.print("Informe n1: ");
+            n1 = entrada.nextInt();
+            System.out.print("Informe n2: ");
+            n2 = entrada.nextInt();
+        
+            //Processamento
+            total = n1 - n2;
         
         
+            //Saída
+            System.out.println("\n\t " + n1 + " - " + n2 + " = " + total);
+        } else if(op == 4){
+            //Douglas Resto da Divisão
+       
+            //Entrada
+            System.out.println("Resto da divisão");
+            System.out.print("Informe n1: ");
+            n1 = entrada.nextInt();
+            System.out.print("Informe n2: ");
+            n2 = entrada.nextInt();
         
+            //Processamento
+            total = n1 % n2;
         
-        
-        
+            //Saida
+            System.out.println("\n\t " + n1 + " % " + n2 + " = " + total);
+        } else {
+            System.out.println("Opção " + op + " inválida!");
+        }
+                                                                
     }
     
     
